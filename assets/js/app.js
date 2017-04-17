a5f5168c5ff50d304b0732d9564c55a26eb157f5
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-04-17 12:29:48 -0500

    agregado de funcionalidad en imagenes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var imagenes = document.getElementsByClassName("box-work");
+/*var imagenes = document.getElementsByClassName("box-work");
 imagenes.addEventListener("onmouseover", mostrarImagenGrande);
 imagenes.addEventListener("onmouseout", regresarTamañoInicial);
 
@@ -9,4 +9,36 @@ function mostrarImagenGrande() {
 
 function regresarTamañoInicial() {
 
+}*/
+
+//PRUEBA1
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+//var img = document.getElementById('myImg');
+var modalImg = document.getElementById("imagenMostrar");
+var captionText = document.getElementById("caption");
+
+var img = [];
+var span = [];
+
+for(var i=0; i < 9; i++){
+  img[i] = document.getElementsByClassName("mostrarImagen")[i];
+  img[i].addEventListener("click", mostrarImagenGrande);
+}
+
+for (var i = 0; i < 9; i++){
+ span[i]= document.getElementsByClassName("close")[0];
+ span[i].addEventListener("click",cerrar);
+}
+
+function mostrarImagenGrande(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// When the user clicks on <span> (x), close the modal
+function cerrar() {
+    modal.style.display = "none";
 }
