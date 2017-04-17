4a09cfbf61e9180fef7b9d64283424bf640b78d3
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-04-16 21:29:27 -0500

    funcionando tercera seccion

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+var imagenes = document.getElementsByClassName("box-work");
+imagenes.addEventListener("onmouseover", mostrarImagenGrande);
+imagenes.addEventListener("onmouseout", regresarTamañoInicial);
+
+
+function mostrarImagenGrande() {
+
+}
+
+function regresarTamañoInicial() {
+
+}
